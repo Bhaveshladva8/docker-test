6f59f0c64b05cc8ce1f69818cafedb928ebd40d9
Nirav bakraniya <bhavesh.ladva@techavidus.com>
2022-12-29 12:24:10 +0530

    New docker compose & yml file

---
+++ docker-test/Controllers/NamesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace docker_test.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NamesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Test", "Nirav", "B", "N"
+        };
+
+        [HttpGet(Name = "GetNames")]
+        public List<string> Get()
+        {
+            return Summaries.ToList();
+        }
+    }
+}
